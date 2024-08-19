6d7c0bcbe7cd9ddde475b6d8f9353553ebfddc46
bakhtiar-mazrur <91288326+bakhtiar-mazrur@users.noreply.github.com>
2024-08-19 13:40:06 +0600

    Initial commit

---
+++ src/main/java/com/hms/controller/HospitalManagementSystem.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hms.controller;
+import com.hms.model.*;
+import com.hms.view.*;
+
+/**
+ *
+ * @author PC
+ */
+public class HospitalManagementSystem {
+
+    public static void main(String[] args) {
+        Home viewHome = new Home();
+        viewHome.setVisible(true);
+    }
+}
