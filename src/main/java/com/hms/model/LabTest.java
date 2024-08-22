7f7a79a77b5843ab2d5e7628e4f5986b12746264
bakhtiar-mazrur <91288326+bakhtiar-mazrur@users.noreply.github.com>
2024-08-22 18:31:02 +0600

    Implemented Polymorphism

--- src/main/java/com/hms/model/LabTest.java
+++ src/main/java/com/hms/model/LabTest.java
@@ -9,7 +9,62 @@ package com.hms.model;
  * @author Bakhtiar Mazrur
  */
 public class LabTest {
+
     protected String title;
     protected double cost;
     protected boolean isAvailable;
+    
+    public LabTest(String title, double cost, boolean isAvailable) {
+        this.title = title;
+        this.cost = cost;
+        this.isAvailable = isAvailable;
+    }
+    
+//    public String returnLabTestInfo() {
+//        String output = "Test name: " + this.getTitle() + " "
+//                + "Cost: " + this.getCost() + " "
+//                + "Availability: " + this.isIsAvailable();
+//        return output;
+//    }
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the cost
+     */
+    public double getCost() {
+        return cost;
+    }
+
+    /**
+     * @param cost the cost to set
+     */
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    /**
+     * @return the isAvailable
+     */
+    public boolean isIsAvailable() {
+        return isAvailable;
+    }
+
+    /**
+     * @param isAvailable the isAvailable to set
+     */
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
 }
